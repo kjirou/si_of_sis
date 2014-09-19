830ac62805d305ae8d54de6af72a78341bec3430
kjirou <kjirou.web@gmail.com>
2014-09-19 22:04:46 +0900

    Move environments to env/development from boot/development

--- boot/development.js
+++ boot/development.js
@@ -1,9 +1,7 @@
 require('coffee-script/register');
 
+require('../env/development');
 
-process.env.NODE_PATH = __dirname + '/..';
-require('module')._initPaths();
-process.env.NODE_ENV = 'development';
 
 var app = require('apps/app');
 app.listen(3000);
